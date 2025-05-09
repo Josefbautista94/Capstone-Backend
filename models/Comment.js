64c40f84d520f5327caac9506cae6fd3aff49343
Jose <josefbautista94@gmail.com>
2025-05-09 03:29:29 -0400

    Added Comment model schema for user-submitted area-based comments

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"; // importing mongoose
+
+const CommentSchema = new mongoose.Schema({
+
+    area: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+
+})
+
+const Comment = mongoose.model("Comment", CommentSchema); // Creating the model, Comment is the name of the model, Mongoose automatically creates a collection named comments in the MongoDB database using this name, It converts "Comment" to lowercase + plural form -> comments
+// Second argument CommentSchema is a blueprint that defines what each document should look like
+
+export default Comment;
